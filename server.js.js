a7fdaf17005b551504b5546758fce8d8292a577d
kulkarnideepak <kulkarni@gmail.com>
2017-09-10 17:01:06 -0700

    changes

--- server.js.js
+++ server.js.js
@@ -1,8 +1,14 @@
+//-- Initialize express 
 var express = require("express");
 var app = express();
 app.configure(function(){
 app.use(express.bodyParser());
 });
+
+//-- URL Route speficiation
 require('./routes')(app);
+
+//-- Start the service
 app.listen(8080);
-console.log("Listening on port 8080");
+console.log("Coffee shop service started on port 8080");
+console.log("========================================")
